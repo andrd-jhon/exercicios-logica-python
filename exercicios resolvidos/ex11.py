788be1f0c180c1070f503ab64f5056b37019838f
andrd-jhon <jv-andrade02@outlook.com>
2024-04-30 20:16:36 -0300

    resolução ate ex 12

---
+++ exercicios resolvidos/ex11.py
@@ -0,0 +1,9 @@
+prova_1_primeiro_b = int(input('Informe a nota da prova nº 1, do primeiro bimestre: '))
+prova_2_primeiro_b = int(input('Informe a nota da prova nº 2, do primeiro bimestre: '))
+prova_1_segundo_b = int(input('Informe a nota da prova nº 1, do primeiro bimestre: '))
+prova_2_segundo_b = int(input('Informe a nota da prova nº 2, do segundo bimestre: '))
+
+media_primeiro_b = (prova_1_primeiro_b + prova_2_primeiro_b) / 2
+media_segundo_b = (prova_1_segundo_b + prova_2_segundo_b) / 2
+
+print(f'A média do aluno é: {(media_primeiro_b + media_segundo_b) / 2}')
---
+++ exercicios resolvidos/ex12.py
@@ -0,0 +1,2 @@
+velocidade_metros_segundo = float(input('Informe a velocidade em metros por segundo: '))
+print(f'A valocidade em km/h é: {velocidade_metros_segundo * 3.6}')
